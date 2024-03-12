72865c00d6b33210c21eb8f6e4530de62e7b14dd
xymtop <writehnmy@vip.qq.com>
2024-03-12 18:23:54 +0700

    删除测试代码

--- Assets/TestConn.cs
+++ Assets/TestConn.cs
@@ -5,7 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestConn : MonoBehaviour
+public class TestConn 
 {
 
     UserService service;
@@ -14,27 +14,10 @@ public class TestConn : MonoBehaviour
     {
         service = new UserService();
     }
-    // Start is called before the first frame update
-    void Start()
+    
+
+    public void Run()
     {
-     /*   for (int i = 1; i < 10; i++)
-        {
 
-      *//*      User admin =service.getUser(i.ToString());
-            Console.WriteLine(admin);
-            System.Console.WriteLine("Hello World!");
-            //  User user = new User();
-            // user._id = i.ToString();
-            //user.username = i.ToString();
-            // user.password = "123456";
-            //service.Registe(user);*//*
-
-        }*/
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
